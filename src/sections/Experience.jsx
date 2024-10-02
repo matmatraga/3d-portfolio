f9b357983076ffcc24a512bdc85f4a77f9bea2b7
matthewraga <matthew.raga@tuitt.com>
2024-10-02 14:07:22 +0800

    add id on work section

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -10,7 +10,7 @@ const Experience = () => {
     const [animationName, setAnimationName] = useState('idle');
 
   return (
-    <section className="c-space my-20">
+    <section className="c-space my-20" id="work">
         <div className="w-full text-white-600">
             <h3 className="head-text">My Work Experience</h3>
 
