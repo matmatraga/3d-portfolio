ac8d51a58877d5990ac463e39ef55e060f27016e
matthewraga <matthew.raga@tuitt.com>
2024-10-02 14:02:56 +0800

    add alert contact

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,117 +1,133 @@
-import { useRef, useState } from "react";
 import emailjs from '@emailjs/browser';
+import { useRef, useState } from 'react';
+
+import useAlert from '../hooks/useAlert.js';
+import Alert from '../components/Alert.jsx';
 
 const Contact = () => {
+  const formRef = useRef();
 
-    const formRef = useRef();
+  const { alert, showAlert, hideAlert } = useAlert();
+  const [loading, setLoading] = useState(false);
 
-    const [loading, setLoading] = useState(false);
+  const [form, setForm] = useState({ name: '', email: '', message: '' });
 
-    const [form, setForm] = useState({
-        name: '',
-        email: '',
-        message: '',
-    })
+  const handleChange = ({ target: { name, value } }) => {
+    setForm({ ...form, [name]: value });
+  };
 
-    const handleChange = ({ target: { name, value }}) => {
-        setForm({...form, [name]: value})
-    }
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
 
-        setLoading(true);
+    emailjs
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: 'Matthew Raga',
+          from_email: form.email,
+          to_email: 'matthewmondoy.raga@gmail.com',
+          message: form.message,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
+      )
+      .then(
+        () => {
+          setLoading(false);
+          showAlert({
+            show: true,
+            text: 'Thank you for your message 😃',
+            type: 'success',
+          });
 
-        try{  
-            await emailjs.send(
-                import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-                import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-                {
-                    from_name: form.name,
-                    to_name: 'Matthew',
-                    from_email: form.email,
-                    to_email: 'matthewmondoy.raga@gmail.com',
-                    message: form.message
-                },
-                import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-            )
-
-            setLoading(false);
-
-            alert('Your message has been sent!')
-
+          setTimeout(() => {
+            hideAlert(false);
             setForm({
-                name: '',
-                email: '',
-                message: '',
-            })
-        }catch(error){
-            setLoading(false);
+              name: '',
+              email: '',
+              message: '',
+            });
+          }, [3000]);
+        },
+        (error) => {
+          setLoading(false);
+          console.error(error);
 
-            console.log(error);
+          showAlert({
+            show: true,
+            text: "I didn't receive your message 😢",
+            type: 'danger',
+          });
+        },
+      );
+  };
 
-            alert('Something went wrong!')
-        }
-    }
+  return (
+    <section className="c-space my-20" id="contact">
+      {alert.show && <Alert {...alert} />}
 
-    return (
-        <section className="c-space my-20">
-            <div className="relative min-h-screen flex items-center justify-center flex-col">
-                <img src="/assets/terminal.png" alt="terminal-bg" className="absolute inset-0 min-h-screen" />
+      <div className="relative min-h-screen flex items-center justify-center flex-col">
+        <img src="/assets/terminal.png" alt="terminal-bg" className="absolute inset-0 min-h-screen" />
 
-                <div className="contact-container">
-                    <h3 className="head-text">Let's talk</h3>
-                    <p className="text-lg text-white-600 mt-3">
-                        Whether you're looking to build a new website, improve your existing platform, or bring a unique project to life, I'm here to help.
-                    </p>
+        <div className="contact-container">
+          <h3 className="head-text">Let's talk</h3>
+          <p className="text-lg text-white-600 mt-3">
+            Whether you’re looking to build a new website, improve your existing platform, or bring a unique project to
+            life, I’m here to help.
+          </p>
 
-                    <form ref={formRef} onSubmit={handleSubmit} className="mt-12 flex flex-col space-y-7">
-                        <label className="space-y-3">
-                            <span className="field-label">Full Name</span>
-                            <input 
-                                type="text"
-                                name="name"
-                                value={form.name}
-                                onChange={handleChange}
-                                required
-                                className="field-input"
-                                placeholder="ex., John Doe"
-                            />
-                        </label>
-                        <label className="space-y-3">
-                            <span className="field-label">Email</span>
-                            <input 
-                                type="email"
-                                name="email"
-                                value={form.email}
-                                onChange={handleChange}
-                                required
-                                className="field-input"
-                                placeholder="ex., johndoe@gmail.com"
-                            />
-                        </label>
-                        <label className="space-y-3">
-                            <span className="field-label">Your message</span>
-                            <textarea 
-                                name="message"
-                                value={form.message}
-                                onChange={handleChange}
-                                required
-                                rows={3}
-                                className="field-input"
-                                placeholder="Let's connect..."
-                            />
-                        </label>
+          <form ref={formRef} onSubmit={handleSubmit} className="mt-12 flex flex-col space-y-7">
+            <label className="space-y-3">
+              <span className="field-label">Full Name</span>
+              <input
+                type="text"
+                name="name"
+                value={form.name}
+                onChange={handleChange}
+                required
+                className="field-input"
+                placeholder="ex., John Doe"
+              />
+            </label>
 
-                        <button className="field-btn" type="submit" disabled={loading}>
-                            {loading ? "Sending..." : "Send Message"}
+            <label className="space-y-3">
+              <span className="field-label">Email address</span>
+              <input
+                type="email"
+                name="email"
+                value={form.email}
+                onChange={handleChange}
+                required
+                className="field-input"
+                placeholder="ex., johndoe@gmail.com"
+              />
+            </label>
 
-                            <img src="/assets/arrow-up.png" alt="arrow-up" className="field-btn_arrow" />
-                        </button>
-                    </form>
-                </div>
-            </div>
-        </section>
-    )
-}
+            <label className="space-y-3">
+              <span className="field-label">Your message</span>
+              <textarea
+                name="message"
+                value={form.message}
+                onChange={handleChange}
+                required
+                rows={3}
+                className="field-input"
+                placeholder="Share your thoughts or inquiries..."
+              />
+            </label>
 
-export default Contact
+            <button className="field-btn" type="submit" disabled={loading}>
+              {loading ? 'Sending...' : 'Send Message'}
+
+              <img src="/assets/arrow-up.png" alt="arrow-up" className="field-btn_arrow" />
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
